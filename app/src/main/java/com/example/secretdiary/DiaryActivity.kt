32d72276c8e2370adb41266acc5b9cddf94a97cf
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-09 14:55:27 +0900

    [Feature] - Extracted variable style

--- app/src/main/java/com/example/secretdiary/DiaryActivity.kt
+++ app/src/main/java/com/example/secretdiary/DiaryActivity.kt
@@ -1,13 +1,21 @@
 package com.example.secretdiary
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class DiaryActivity : AppCompatActivity() {
+    private val diaryEditText: EditText by lazy{
+        findViewById<EditText>(R.id.diaryEditText)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
 
+        /* Bring the MainActivity Information from `MainActivity.kt` SharePreferences */
+        val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
 
     }
 }
