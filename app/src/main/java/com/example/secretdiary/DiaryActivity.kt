4e9148efc1b9839549332c7066daa3bef6e9b7d5
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-09 14:56:12 +0900

    [Feature] - Local variable styles without Generic

--- app/src/main/java/com/example/secretdiary/DiaryActivity.kt
+++ app/src/main/java/com/example/secretdiary/DiaryActivity.kt
@@ -6,13 +6,11 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class DiaryActivity : AppCompatActivity() {
-    private val diaryEditText: EditText by lazy{
-        findViewById<EditText>(R.id.diaryEditText)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
+
+        val diaryEditText: EditText = findViewById(R.id.diaryEditText)
 
         /* Bring the MainActivity Information from `MainActivity.kt` SharePreferences */
         val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
